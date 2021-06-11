d181902b4262594caf9fb0501ddc04c0e501bc27
rotrojan <rotrojan@student.42.fr>
2021-06-11 20:23:14 +0200

    WIP

--- srcs/requirements/mariadb/run.sh
+++ srcs/requirements/mariadb/run.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
 chown -R mysql:mysql /var/lib/mysql
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-# mysql << EOF
-# CREATE DATABASE ${MYSQL_DATABASE};
-# GRANT ALL ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@${WORDPRESS_DB_HOST} WITH GRANT OPTION;
-# FLUSH PRIVILEGES;
-# EOF
-# mysql_install_db
+chmod -R 755 /var/lib/mysql
+mysql_install_db --defaults-file=~/.my.cnf
+# service mysql start
+mysql --user=root << _EOF_
+UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+_EOF_
+mysql << _EOF_
+
+_EOF_
+service mysql stop
 
 
 [ "$DEBUG" -eq 1 ] && exec /bin/bash || exec "$@"
--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -4,7 +4,9 @@ wget https://wordpress.org/latest.tar.gz
 tar -xf latest.tar.gz
 rm -rf latest.tar.gz
 mkdir -p /var/www/html/localhost/
-mv /wordpress /var/www/html/localhost/wordpress
+mv /tmp/wp-config.php /wordpress/
+mv /wordpress/ /var/www/html/localhost/wordpress/
 mkdir -p /run/php
+echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 [ "$DEBUG" -eq 1 ] && exec /bin/bash || exec "$@"
