32685ac01f442681c57baef311e2089f9436b286
rotrojan <rotrojan@student.42.fr>
2022-04-23 17:36:40 +0200

    WIP

--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-#wp core install --url=localhost --title=my_awesome_wordpress --admin-user=db_username --admin-pass=db_password
-#wp db create --path=$WP_DIR --allow-root
-
-#echo "<?php phpinfo(); ?>" > /var/www/html/localhost/wordpress/phpinfo.php
-
-# maybe enable communication between mariadb and wordpress
-#echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+#[ "wp core is-installed --path=/var/www/inception/wordpress/" = 0 ]
+wp core install \
+	--path=/var/www/inception/wordpress \
+	--url=rotrojan.42.fr \
+	--admin_user=roycampbell \
+	--admin_email=roycampbell@foxhound.gov \
+	--title=Inception
 
 exec "$@"
