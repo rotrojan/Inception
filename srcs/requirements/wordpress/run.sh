00c3fcfb51c44a9a615a51f1abad0dd6468bac62
rotrojan <rotrojan@student.42.fr>
2022-04-24 18:09:53 +0200

    Cannot use CGI

--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
-#[ "wp core is-installed --path=/var/www/inception/wordpress/" = 0 ]
-wp core install \
-	--path=/var/www/inception/wordpress \
-	--url=rotrojan.42.fr \
-	--admin_user=roycampbell \
-	--admin_email=roycampbell@foxhound.gov \
-	--title=Inception
+sleep 5
+until mysqladmin -u roycampbell --password=roycampbell_password -h mariadb ping; do
+	sleep 1
+done
+wp core is-installed --path=/var/www/inception/wordpress/ ||
+#wp core install \
+#	--path=/var/www/inception/wordpress \
+#	--url=rotrojan.42.fr \
+#	--admin_user=roycampbell \
+#	--admin_password=roycampbell_password \
+#	--admin_email=roycampbell@foxhound.gov \
+#	--skip-email \
+#	--title=Inception
 
 exec "$@"
